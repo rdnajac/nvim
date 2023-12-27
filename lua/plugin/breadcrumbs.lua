a1c24954dbc167b378b5cc66752c0d75c4f1d978
Ryan D. Najac <rdn2108@columbia.edu>
2023-12-26 22:10:21 -0500

    add trouble

--- lua/plugin/breadcrumbs.lua
+++ lua/plugin/breadcrumbs.lua
@@ -1,10 +1,23 @@
+--- breadcrumbs.lua
+-- Configuration for the `LunarVim/breadcrumbs.nvim` plugin.
+-- @module breadcrumbs
 local M = {
+  --- The name of the plugin repository.
+  -- @string
   "LunarVim/breadcrumbs.nvim",
+
+  --- The specific commit hash of the plugin to use.
+  -- @string
   commit = "1033b354f65206793831207d5c9047fc059e35c3",
 }
 
+---
+-- Configures the behavior of the `LunarVim/breadcrumbs.nvim` plugin.
+-- @function config
 function M.config()
+  --- Set up the `breadcrumbs` plugin.
   require("breadcrumbs").setup()
 end
 
 return M
+
---
+++ lua/plugin/trouble.lua
@@ -0,0 +1,10 @@
+local M = {
+ "folke/trouble.nvim",
+}
+
+function M.config()
+  require("trouble").setup()
+end
+
+return M
+
