252f306f64fb98d209cc4895dc90b638cb736922
Ryan D. Najac <rdn2108@columbia.edu>
2024-02-08 22:29:46 -0500

    updates

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
 --- init.lua
--- This is the main configuration file for Neovim, written in Lua.
+-- This is the main configuration file for Neovim.
 -- It is responsible for loading various plugin.configuration modules.
 
 -- Core Modules
@@ -8,59 +8,48 @@ require "options"      -- Sets up Neovim options and general settings.
 require "keymaps"      -- Configures key mappings.
 require "autocmds"     -- Defines autocommands for automated tasks.
 
--- Plugin Specifications
--- The 'spec' function is used to specify plugin configurations (???).
-
--- User Interface Enhancements
-spec "plugin.colorscheme"    -- Color scheme customization
-spec "plugin.lualine"        -- Status line configuration
-
-spec "plugin.devicons"       -- File icons
-
-spec "plugin.alpha"          -- Customizable start screen
-spec "plugin.indentline"     -- Visual indentation guides
+-- Git Integration
 spec "plugin.diffview"       -- Git diff visualization
+spec "plugin.gitsigns"       -- Git integration with editor signs
+spec "plugin.neogit"         -- Enhanced Git experience (???)
 
--- File and Project Navigation
+-- User Interface Distractions
+spec "plugin.alpha"          -- Customizable start screen
+spec "plugin.colorscheme"    -- Color scheme customization
+spec "plugin.illuminate"     -- Highlighting of current word usage
+spec "plugin.indentline"     -- Visual indentation guides
+spec "plugin.lualine"        -- Status line configuration 
+spec "plugin.whichkey"       -- Keybinding assistance
+
+-- QOL
+spec "plugin.bufdelete"      -- Buffer deletion without closing windows
+spec "plugin.comment"        -- Easy commenting of code blocks
+spec "plugin.toggleterm"     -- Terminal integration
+
+-- File Explorer 
+spec "plugin.navic"          -- Shows current code context in the tabline with cute little glyphs
+spec "plugin.netrw"          -- Adds little file/folder/symlink icons 
 spec "plugin.nvimtree"       -- File explorer
-spec "plugin.telescope"      -- Fuzzy finder
-spec "plugin.telescope-tabs" -- Tab management with Telescope
 spec "plugin.project"        -- Project management
+spec "plugin.telescope"      -- Fuzzy finder
 
 -- Coding Completion and Syntax Tools
 spec "plugin.treesitter"     -- Enhanced syntax highlighting and parsing
-spec "plugin.autopairs"      -- Auto pairing of brackets and quotes
-spec "plugin.comment"        -- Easy commenting of code blocks
 spec "plugin.cmp"            -- Completion engine configurations
-
--- Git Integration
-spec "plugin.gitsigns"       -- Git integration with editor signs
-spec "plugin.neogit"         -- Enhanced Git experience
 
 -- Language Server Protocol (LSP) and Development Tools
 spec "plugin.lspconfig"      -- Language server configurations
 spec "plugin.mason"          -- LSP servers and tools management
 spec "plugin.null-ls"        -- Integration of linters and formatters
-spec "plugin.schemastore"    -- JSON schema settings
-spec "plugin.illuminate"     -- Highlighting of current word usage
-spec "plugin.navic"          -- Navigation enhancements
-
--- Key Mapping and Commands
-spec "plugin.whichkey"       -- Keybinding assistance
-
--- Terminal and Shell Integration
-spec "plugin.toggleterm"     -- Terminal integration
 
 -- Utility and Miscellaneous
-spec "plugin.breadcrumbs"    -- Breadcrumb navigation
-spec "plugin.netrw"          -- Built-in file explorer
-spec "plugin.bufdelete"      -- Buffer deletion without closing windows
+spec "plugin.markdown-preview" -- Render md files in browser
 
 -- New and untested
 spec "plugin.trouble"
-spec "plugin.text-to-colorscheme"
-spec "plugin.markdown-preview"
 
 -- Lazy Loading
 require "plugin._lazy"        -- Handles lazy loading of certain plugins or configurations.
 
+-- Force color scheme
+--require('colors').setup()
--- lua/colors.lua
+++ lua/colors.lua
@@ -1,12 +1,41 @@
-return {
-  -- the colorscheme should be available when starting Neovim
-  {
-    "folke/tokyonight.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      -- load the colorscheme here
-      vim.cmd([[colorscheme tokyonight]])
-    end,
-  },
+local colors = {
+    bg = '#000000',
+    fg = '#39ff14',
+    blue = '#14afff',
+    magenta = '#ff69ff',
 }
+
+local function setup()
+    vim.api.nvim_command('highlight clear')
+    if vim.fn.exists('syntax_on') then
+        vim.api.nvim_command('syntax reset')
+    end
+    vim.o.termguicolors = true
+    vim.g.colors_name = "custom_scheme"
+
+    -- General colors
+    vim.api.nvim_set_hl(0, 'Normal', { bg = "000000" })
+    vim.api.nvim_set_hl(0, 'Comment', { fg = colors.fg })
+    vim.api.nvim_set_hl(0, 'Constant', { fg = colors.fg })
+
+    -- Syntax highlighting
+    vim.api.nvim_set_hl(0, 'Statement', { fg = colors.blue })
+    vim.api.nvim_set_hl(0, 'PreProc', { fg = colors.blue })
+    vim.api.nvim_set_hl(0, 'Type', { fg = colors.magenta })
+    vim.api.nvim_set_hl(0, 'Special', { fg = colors.magenta })
+
+    -- UI Elements
+    vim.api.nvim_set_hl(0, 'CursorLine', { bg = '#202020' })
+    vim.api.nvim_set_hl(0, 'LineNr', { fg = '#505050', bg = colors.bg })
+    vim.api.nvim_set_hl(0, 'StatusLine', { fg = colors.fg, bg = '#202020' })
+    vim.api.nvim_set_hl(0, 'StatusLineNC', { fg = '#505050', bg = '#202020' })
+    vim.api.nvim_set_hl(0, 'VertSplit', { fg = '#505050', bg = colors.bg })
+    vim.api.nvim_set_hl(0, 'MatchParen', { bg = '#303030' })
+
+    -- Additional elements can be customized as needed
+end
+
+return {
+    setup = setup,
+}
+
