7cda71f9810082c33f25ca2118161e6a3c3c65b0
Ryan D. Najac <ryan.najac@columbia.edu>
2024-09-24 21:02:34 -0400

    properly returns the module

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -1,80 +1,71 @@
-{
-  {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v4.x',
-    lazy = true,
-    config = false,
-  },
-  {
-    'williamboman/mason.nvim',
-    lazy = false,
-    config = true,
-  },
-  -- Autocompletion
-  {
-    'hrsh7th/nvim-cmp',
-    event = 'InsertEnter',
-    dependencies = {
-      {'L3MON4D3/LuaSnip'},
-    },
-    config = function()
-      local cmp = require('cmp')
-      cmp.setup({
-        sources = {
-          {name = 'nvim_lsp'},
-        },
-        mapping = cmp.mapping.preset.insert({
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-d>'] = cmp.mapping.scroll_docs(4),
-        }),
-        snippet = {
-          expand = function(args)
-            vim.snippet.expand(args.body)
-          end,
-        },
-      })
-    end
-  },
-  -- LSP
-  {
-    'neovim/nvim-lspconfig',
-    cmd = {'LspInfo', 'LspInstall', 'LspStart'},
-    event = {'BufReadPre', 'BufNewFile'},
-    dependencies = {
-      {'hrsh7th/cmp-nvim-lsp'},
-      {'williamboman/mason.nvim'},
-      {'williamboman/mason-lspconfig.nvim'},
-    },
-    config = function()
-      local lsp_zero = require('lsp-zero')
-      
-      lsp_zero.on_attach(function(client, bufnr)
-        -- see :help lsp-zero-keybindings
-        -- to learn the available actions
-        lsp_zero.default_keymaps({buffer = bufnr})
-      end)
-
-      lsp_zero.extend_lspconfig({
-        sign_text = true,
-        capabilities = require('cmp_nvim_lsp').default_capabilities()
-      })
-
-      require('mason').setup({})
-      require('mason-lspconfig').setup({
-        handlers = {
-          function(server_name)
-            require('lspconfig')[server_name].setup({})
-          end,
-          lua_ls = function()
-            require('lspconfig').lua_ls.setup({
-              on_init = function(client)
-                lsp_zero.nvim_lua_settings(client, {})
-              end,
-            })
-          end,
-        },
-      })
-    end
-  }
+local M = {
+	"VonHeikemen/lsp-zero.nvim",
+	branch = "v4.x",
+	lazy = true,
+	config = false,
+	dependencies = {
+		{
+			"neovim/nvim-lspconfig",
+			cmd = { "LspInfo", "LspInstall", "LspStart" },
+			event = { "BufReadPre", "BufNewFile" },
+			dependencies = {
+				{ "hrsh7th/cmp-nvim-lsp" },
+				{ "williamboman/mason.nvim" },
+				{ "williamboman/mason-lspconfig.nvim" },
+			},
+		},
+		{
+			"hrsh7th/nvim-cmp",
+			event = "InsertEnter",
+			dependencies = {
+				{ "L3MON4D3/LuaSnip" },
+			},
+		},
+		{
+			"williamboman/mason.nvim",
+			lazy = false,
+			config = true,
+		},
+	},
 }
+
+function M.config()
+	local lsp_zero = require("lsp-zero")
+
+	lsp_zero.on_attach(function(client, bufnr)
+		lsp_zero.default_keymaps({ buffer = bufnr })
+	end)
+
+	require("mason").setup({})
+	require("mason-lspconfig").setup({
+		handlers = {
+			lsp_zero.default_setup,
+			lua_ls = function()
+				require("lspconfig").lua_ls.setup({
+					on_init = function(client)
+						lsp_zero.nvim_lua_settings(client, {})
+					end,
+				})
+			end,
+		},
+	})
+
+	local cmp = require("cmp")
+	cmp.setup({
+		sources = {
+			{ name = "nvim_lsp" },
+		},
+		mapping = cmp.mapping.preset.insert({
+			["<C-Space>"] = cmp.mapping.complete(),
+			["<C-u>"] = cmp.mapping.scroll_docs(-4),
+			["<C-d>"] = cmp.mapping.scroll_docs(4),
+		}),
+		snippet = {
+			expand = function(args)
+				require("luasnip").lsp_expand(args.body)
+			end,
+		},
+	})
+end
+
+return M
