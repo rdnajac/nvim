aa56b8271401decdb40fd60b4a02f94a1c93fbe1
Ryan D. Najac <ryan.najac@columbia.edu>
2024-09-26 11:33:08 -0400

    revert some changes

--- lua/lazyvim/plugins/init.lua
+++ lua/lazyvim/plugins/init.lua
@@ -12,4 +12,5 @@ require("lazyvim.config").init()
 
 return {
   { "folke/lazy.nvim", version = "*" },
+  { "LazyVim/LazyVim", priority = 10000, lazy = false, opts = {}, cond = true, version = "*" },
 }
