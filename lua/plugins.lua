02d804aee27daa604ebaa78ba5da4ce8fbcee47f
Ryan D. Najac <ryan.najac@columbia.edu>
2024-11-17 17:02:45 -0500

    rework configs

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,6 +1,19 @@
 return {
-  { 'folke/tokyonight.nvim', },
-  { dir = '~/.vim' },
-  { dir = '~/.vim/.plugged/tabular' },
-  { import = 'lsp' },
+  { 'neovim/nvim-lspconfig' },
+  { 'VonHeikemen/lsp-zero.nvim', opts = {} },
+  { 'williamboman/mason.nvim', opts = {} },
+  {
+    'williamboman/mason-lspconfig.nvim',
+    opts = {
+      ensure_installed = {
+        'lua_ls',
+      },
+      handlers = {
+        function(server_name)
+          require('lspconfig')[server_name].setup({})
+        end,
+        {},
+      },
+    },
+  },
 }
