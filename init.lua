6d0ab5cddca3a26cf8775d1f9d6cf980f0e67207
Ryan D. Najac <ryan.najac@columbia.edu>
2024-11-18 12:52:11 -0500

    use bootstrap script with curl if needed

--- init.lua
+++ init.lua
@@ -2,29 +2,15 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = '\\'
 
--- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath('config') .. '/.lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  print('Lazy.nvim not found. Installing...')
-  -- local bootstrap_script = vim.fn.system('curl -s https://raw.githubusercontent.com/folke/lazy.nvim/main/bootstrap.lua')
-  -- loadstring(bootstrap_script)()
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+  load(vim.fn.system("curl -s https://raw.githubusercontent.com/folke/lazy.nvim/main/bootstrap.lua"))()
+else
+  vim.opt.rtp:prepend(lazypath)
 end
 
-vim.opt.rtp:prepend(lazypath)
-
 -- Set up lazy.nvim and configure plugins
-local lazy_config = require('config.lazy')
-require('lazy').setup(lazy_config)
+require('lazy').setup(require('config.lazy'))
+
 vim.cmd('colorscheme tokyonight')
--- require('lsp')
