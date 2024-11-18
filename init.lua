a656263a4e3672f6c83395d9b74c565471dfba20
Ryan D. Najac <ryan.najac@columbia.edu>
2024-11-18 12:37:45 -0500

    mv lazy config

--- init.lua
+++ init.lua
@@ -3,71 +3,28 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = '\\'
 
 -- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
+local lazypath = vim.fn.stdpath('config') .. '/.lazy/lazy.nvim'
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
   print('Lazy.nvim not found. Installing...')
-  local bootstrap_script = vim.fn.system('curl -s https://raw.githubusercontent.com/folke/lazy.nvim/main/bootstrap.lua')
-  loadstring(bootstrap_script)()
+  -- local bootstrap_script = vim.fn.system('curl -s https://raw.githubusercontent.com/folke/lazy.nvim/main/bootstrap.lua')
+  -- loadstring(bootstrap_script)()
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 
 vim.opt.rtp:prepend(lazypath)
 
 -- Set up lazy.nvim and configure plugins
-require('lazy').setup({
-  spec = {
-    { 'folke/tokyonight.nvim' },
-    { 'VonHeikemen/lsp-zero.nvim' },
-    { 'neovim/nvim-lspconfig' },
-    { 'hrsh7th/nvim-cmp'},
-    { 'hrsh7th/cmp-nvim-lsp'},
-    { 'williamboman/mason.nvim' },
-    { 'williamboman/mason-lspconfig.nvim' },
-  },
-  local_spec = false,
-  lockfile = vim.fn.stdpath('config') .. '/.lazy-lock.json', -- lockfile generated after running update.
-  pkg = { enabled = false, },  -- see: `pkg.json`
-  install = {
-    missing = true,
-    -- colorscheme = { 'tokyonight-night' },
-  },
-  ui = {
-    wrap = false,
-    border = 'rounded',
-    icons = {
-      cmd = '⌘',
-      config = '🛠',
-      event = '📅',
-      ft = '📂',
-      init = '⚙',
-      keys = '🗝',
-      plugin = '🔌',
-      runtime = '💻',
-      require = '🌙',
-      source = '📄',
-      start = '🚀',
-      task = '📌',
-      lazy = '💤',
-    },
-  },
-  change_detection = {
-    enabled = true, -- automatically check for config file changes and reload the ui
-    notify = true, -- get a notification when changes are found
-  },
-  performance = {
-    reset_packpath = true, -- reset the package path to improve startup time
-    rtp = {
-      disabled_plugins = {
-        'gzip',
-        -- "matchit",
-        -- "matchparen",
-        -- "netrwPlugin",
-        'tarPlugin',
-        'tohtml',
-        'tutor',
-        'zipPlugin',
-      },
-    },
-  },
-})
+local lazy_config = require('config.lazy')
+require('lazy').setup(lazy_config)
 vim.cmd('colorscheme tokyonight')
-require('lsp')
+-- require('lsp')
