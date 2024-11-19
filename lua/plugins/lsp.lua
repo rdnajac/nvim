8e46a97b49392a5c1a14da6b1e65ef95ddadf4fe
Ryan D. Najac <ryan.najac@columbia.edu>
2024-11-18 20:02:55 -0500

    mv cmp and use lua-lsp defaults

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,77 +1,44 @@
 return {
   {
-    'williamboman/mason.nvim',
-    lazy = false,
-    opts = {},
-  },
-
-  -- Autocompletion
-  {
-    'hrsh7th/nvim-cmp',
-    event = 'InsertEnter',
-    config = function()
-      local cmp = require('cmp')
-
-      cmp.setup({
-        sources = {
-          { name = 'nvim_lsp' },
-        },
-        mapping = cmp.mapping.preset.insert({
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-d>'] = cmp.mapping.scroll_docs(4),
-        }),
-        snippet = {
-          expand = function(args)
-            vim.snippet.expand(args.body)
-          end,
-        },
-      })
-    end,
-  },
-
-  -- LSP
-  {
     'neovim/nvim-lspconfig',
     cmd = { 'LspInfo', 'LspInstall', 'LspStart' },
     event = { 'BufReadPre', 'BufNewFile' },
     dependencies = {
+      { 'VonHeikemen/lsp-zero.nvim' },
       { 'hrsh7th/cmp-nvim-lsp' },
-      { 'williamboman/mason.nvim' },
-      { 'williamboman/mason-lspconfig.nvim' },
+      { 'williamboman/mason.nvim', opts = {} },
+      {
+        'williamboman/mason-lspconfig.nvim',
+        opts = {
+          ensure_installed = { 'lua_ls' },
+          handlers = {
+            -- applies to every language server without a "custom handler"
+            function(server_name)
+              -- TODO: add print
+              require('lspconfig')[server_name].setup({})
+            end,
+          },
+        },
+      },
     },
-    init = function()
-      -- Reserve a space in the gutter
-      -- This will avoid an annoying layout shift in the screen
-      vim.opt.signcolumn = 'yes'
-    end,
+
     config = function()
+      local lsp_zero = require('lsp-zero')
       local lsp_defaults = require('lspconfig').util.default_config
 
       -- Add cmp_nvim_lsp capabilities settings to lspconfig
       -- This should be executed before you configure any language server
+      -- TODO: add print
       lsp_defaults.capabilities =
         vim.tbl_deep_extend('force', lsp_defaults.capabilities, require('cmp_nvim_lsp').default_capabilities())
 
-      -- LspAttach is where you enable features that only work
-      -- if there is a language server active in the file
+      -- enable features that only work if there is an active language server
       vim.api.nvim_create_autocmd('LspAttach', {
         desc = 'LSP actions',
         callback = function(event)
           -- see :help lsp-zero-keybindings
           lsp_zero.default_keymaps({ buffer = event.buf })
         end,
-      })
-
-      require('mason-lspconfig').setup({
-        ensure_installed = {},
-        handlers = {
-          -- this first function is the "default handler"
-          -- it applies to every language server without a "custom handler"
-          function(server_name)
-            require('lspconfig')[server_name].setup({})
-          end,
-        },
       })
     end,
   },
