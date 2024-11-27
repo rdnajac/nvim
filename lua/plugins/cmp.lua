f1b24c1eb946d8028a1e205e84a956017335617f
Ryan D. Najac <ryan.najac@columbia.edu>
2024-11-27 15:57:10 -0500

    clean up cmp config

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -4,30 +4,7 @@ return {
   dependencies = {
     'hrsh7th/cmp-nvim-lsp',
     'hrsh7th/cmp-buffer',
-    -- 'hrsh7th/cmp-cmdline',
     'hrsh7th/cmp-path',
     { 'zbirenbaum/copilot-cmp', opts = {} },
-  },
-  opts = {
-    snippet = {
-      expand = function(args)
-        vim.snippet.expand(args.body)
-      end,
-    }, -- end snippet
-    formatting = {
-      expandable_indicator = true,
-      fields = { 'menu', 'abbr' }, -- rm 'kind'
-      format = function(entry, item)
-        local menu_icon = require('user.icons').cmp
-        item.menu = menu_icon[entry.source.name]
-        return item
-      end,
-    }, -- end formatting
-    sources = {
-      { name = 'copilot' },
-      { name = 'nvim_lsp' },
-      { name = 'buffer' },
-      { name = 'path' },
-    }, -- end sources
   },
 }
