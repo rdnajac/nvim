1b124771b3fd18aa5061e133b8543df24260f157
Ryan D. Najac <ryan.najac@columbia.edu>
2024-12-02 13:24:52 -0500

    update configs

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,1 +1,2 @@
+--- config/autocmds.lua
 local au = vim.api.nvim_create_autocmd
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
+--- config/keymaps
 local map = vim.keymap.set
 
 map('n', '<localleader>hi', "<cmd>echo 'hi =)'<cr>", { noremap = true })
+
+-- add vim keymaps
+-- vim.cmd[[ source ~/.vim/plugin/config/keymaps.vim ]]
