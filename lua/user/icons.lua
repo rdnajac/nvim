5940b0adb52672de2558a4198e9ab5b30a880e74
Ryan D. Najac <ryan.najac@columbia.edu>
2024-12-02 13:25:04 -0500

    add icons

---
+++ lua/user/icons.lua
@@ -0,0 +1,30 @@
+return {
+  cmp = {
+    nvim_lsp = '👾',
+    buffer = '📝',
+    path = '💽',
+    tmux = '💻',
+    copilot = '🚀',
+  },
+  lazy = {
+    cmd = '⌘',
+    config = '🛠',
+    event = '📅',
+    ft = '📂',
+    init = '⚙',
+    keys = '🗝',
+    plugin = '🔌',
+    runtime = '💻',
+    require = '🌙',
+    source = '📄',
+    start = '🚀',
+    task = '📌',
+    lazy = '💤',
+  },
+  lsp = {
+    error = '🔥',
+    warn = '💩',
+    hint = '👾',
+    info = '🧠',
+  },
+}
