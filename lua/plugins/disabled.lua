7216756fb58d642ffb05680f4d1db25ff428a052
Ryan D. Najac <ryan.najac@columbia.edu>
2024-12-03 00:55:41 -0500

    use mini files

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,15 +1,15 @@
 --- lua/plugins/disabled.lua
 return {
   -- colorscheme
-  { 'catppuccin/nvim',                     enabled = false },
+  { 'catppuccin/nvim', enabled = false },
 
   -- editor
-  { 'nvim-neo-tree/neo-tree.nvim',         enabled = false, },
-  { 'MagicDuck/grug-far.nvim',             enabled = false, },
+  { 'nvim-neo-tree/neo-tree.nvim', enabled = false },
+  { 'MagicDuck/grug-far.nvim', enabled = false },
 
   -- ui
   { 'lukas-reineke/indent-blankline.nvim', enabled = false },
-  { 'folke/noice.nvim',                    enabled = false },
-  { 'stevearc/dressing.nvim',              enabled = false },
-  { 'MunifTanjim/nui.nvim',                enabled = false },
+  { 'folke/noice.nvim', enabled = false },
+  { 'stevearc/dressing.nvim', enabled = false },
+  { 'MunifTanjim/nui.nvim', enabled = false },
 }
