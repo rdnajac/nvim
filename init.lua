66cc9eefc5e175123e5a3fdd7be55380a2498ad8
Ryan D. Najac <ryan.najac@columbia.edu>
2024-12-02 21:21:26 -0500

    add lazyvim extras

--- init.lua
+++ init.lua
@@ -1,6 +1,10 @@
 --- init.lua
 LAZY_SPEC = {
   { 'LazyVim/LazyVim', import = 'lazyvim.plugins' },
+  { import = "lazyvim.plugins.extras.coding.mini-comment" },
+  { import = "lazyvim.plugins.extras.coding.mini-surround" },
+  { import = "lazyvim.plugins.extras.editor.harpoon2" },
+  { import = "lazyvim.plugins.extras.formatting.prettier" },
   { import = 'plugins' },
 }
 
