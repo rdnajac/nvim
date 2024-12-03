573e85e86efe3530fca442ada26d89c03d749ff8
Ryan D. Najac <ryan.najac@columbia.edu>
2024-12-03 04:41:36 -0500

    add extras

--- init.lua
+++ init.lua
@@ -1,16 +1,34 @@
 --- init.lua
-local plug = table.insert
-
-LAZY_SPEC = { { import = 'plugins' } }
+LAZY_SPEC = {}
 
 if os.getenv('LAZY') == 'true' then
-  plug(LAZY_SPEC, { 'LazyVim/LazyVim', import = 'lazyvim.plugins' })
-  plug(LAZY_SPEC, { import = 'lazyvim.plugins.extras.ai.copilot' })
-  plug(LAZY_SPEC, { import = 'lazyvim.plugins.extras.coding.mini-comment' })
-  plug(LAZY_SPEC, { import = 'lazyvim.plugins.extras.coding.mini-surround' })
-  plug(LAZY_SPEC, { import = 'lazyvim.plugins.extras.editor.mini-files' })
-  -- plug(LAZY_SPEC, { import = 'lazyvim.plugins.extras.formatting.prettier' })
+  LAZY_SPEC = {
+    { 'LazyVim/LazyVim', import = 'lazyvim.plugins' },
+    { import = 'lazyvim.plugins.extras.ai.copilot' },
+    { import = 'lazyvim.plugins.extras.coding.mini-comment' },
+    { import = 'lazyvim.plugins.extras.coding.mini-surround' },
+    { import = 'lazyvim.plugins.extras.editor.mini-files' },
+    { import = 'lazyvim.plugins.extras.editor.mini-move' },
+    -- { import = 'lazyvim.plugins.extras.formatting.prettier' })
+    { import = 'lazyvim.plugins.extras.lang.clangd' },
+    { import = 'lazyvim.plugins.extras.lang.cmake' },
+    { import = 'lazyvim.plugins.extras.lang.docker' },
+    { import = 'lazyvim.plugins.extras.lang.git' },
+    { import = 'lazyvim.plugins.extras.lang.markdown' },
+    { import = 'lazyvim.plugins.extras.lang.ocaml' },
+    { import = 'lazyvim.plugins.extras.lang.python' },
+    { import = 'lazyvim.plugins.extras.lang.r' },
+    { import = 'lazyvim.plugins.extras.lang.toml' },
+    { import = 'lazyvim.plugins.extras.lang.yaml' },
+    { import = 'lazyvim.plugins.extras.ui.edgy' },
+    { import = 'lazyvim.plugins.extras.util.dot' },
+    { import = 'lazyvim.plugins.extras.util.gitui' },
+    { import = 'plugins' },
+  }
 else
+  LAZY_SPEC = {
+    { import = 'plugins' },
+  }
   require('user.autocmds')
   require('user.keymaps')
   require('user.options')
