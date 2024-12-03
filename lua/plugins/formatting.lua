c149eecb00ba90532799b93749d4427d8d7a0802
Ryan D. Najac <ryan.najac@columbia.edu>
2024-12-03 01:14:33 -0500

    clean up formatting

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -1,19 +1,14 @@
 --- lua/plugin/formatting.lua
 return {
   'stevearc/conform.nvim',
-  event = { 'BufWritePre' },
-  cmd = { 'ConformInfo' },
   opts = {
     formatters_by_ft = {
       lua = { 'stylua' },
       sh = { 'shfmt' },
       python = { 'black' },
     },
-    default_format_opts = {
-      lsp_format = 'fallback',
-    },
+    -- init = function()
+    --   vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+    -- end,
   },
-  init = function()
-    vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-  end,
 }
