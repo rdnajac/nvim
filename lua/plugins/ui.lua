d4038759560e1d35194b05b88c722fa723cb99d4
Ryan D. Najac <ryan.najac@columbia.edu>
2024-12-05 14:47:46 -0500

    configure dashboard

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -14,21 +14,25 @@ return {
     'folke/snacks.nvim',
     opts = {
       dashboard = {
+        -- cwd = true,
         preset = {
           keys = {
-            { icon = ' ', key = 'f', desc = 'Find File', action = ":lua Snacks.dashboard.pick('files')" },
-            { icon = ' ', key = 'g', desc = 'Find Text', action = ":lua Snacks.dashboard.pick('live_grep')" },
-            { icon = ' ', key = 'r', desc = 'Recent Files', action = ":lua Snacks.dashboard.pick('oldfiles')" },
-            {
-              icon = ' ',
-              key = 'c',
-              desc = 'Config',
-              action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
-            },
-            { icon = ' ', key = 's', desc = 'Restore Session', section = 'session' },
-            { icon = '󰒲 ', key = 'L', desc = 'Lazy', action = ':Lazy', enabled = package.loaded.lazy ~= nil },
-            { icon = ' ', key = 'q', desc = 'Quit', action = ':qa' },
+            { icon = '🔍 ', key = 'f', desc = 'Find File', action = ":lua Snacks.dashboard.pick('files')" },
+            { icon = '📝 ', key = 't', desc = 'Find Text', action = ":lua Snacks.dashboard.pick('live_grep')" },
+            { icon = '📑 ', key = 'r', desc = 'Recent Files', action = ":lua Snacks.dashboard.pick('oldfiles')" },
+            -- stylua: ignore start
+            { icon = '🛠 ', key = 'c', desc = 'Config', action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})", },
+            -- stylua: ignore end
+            { icon = '🔮 ', key = 's', desc = 'Restore Session', section = 'session' },
+            { icon = '💤 ', key = 'l', desc = 'Lazy', action = ':Lazy' },
+            { icon = '👾 ', key = 'x', desc = 'Lazy Extras', action = ':LazyExtras' },
+            { icon = '❌ ', key = 'q', desc = 'Quit', action = ':qa' },
           },
+        },
+        formats = {
+          key = function(item)
+            return { { '[ ', hl = 'special' }, { item.key, hl = 'key' }, { ' ]', hl = 'special' } }
+          end,
         },
         sections = {
           {
@@ -37,19 +41,17 @@ return {
             padding = 1,
             width = 69,
           },
-          -- { icon = ' ', title = 'Keymaps', section = 'keys', indent = 2, padding = 1 },
-          -- { icon = ' ', title = 'Recent Files', section = 'recent_files', indent = 2, padding = 1 },
-          -- { icon = ' ', title = 'Projects', section = 'projects', indent = 2, padding = 1 },
-          { section = 'keys', indent = 2, padding = 1 },
-          { section = 'recent_files', indent = 2, padding = 1 },
-          { section = 'projects', indent = 2, padding = 1 },
+          { section = 'keys', padding = 1 },
+          { section = 'recent_files', padding = 1 },
+          { section = 'projects', padding = 1 },
           { section = 'startup' },
+          {},
         },
       },
     },
     keys = {
       {
-        '<localleader><space>',
+        '<localleader>\\',
         function()
           require('snacks').dashboard.open()
         end,
