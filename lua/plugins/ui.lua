d07811c0b8ba7e14ccd3f2ba2c96371922fba9d1
Ryan D. Najac <ryan.najac@columbia.edu>
2024-12-05 13:09:16 -0500

    merge changes

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -2,15 +2,34 @@
 return {
   {
     'nvim-lualine/lualine.nvim',
-    opts = function(_, opts)
-      opts.tabline = opts.sections
-      opts.sections = {}
-    end,
+    enabled = false,
+    -- opts = function(_, opts)
+    --   opts.tabline = opts.sections
+    --   opts.sections = {}
+    --   opts.inactive_sections = {}
+    --   opts.always_show_tabline = false
+    -- end,
   },
   {
     'folke/snacks.nvim',
     opts = {
       dashboard = {
+        preset = {
+          keys = {
+            { icon = ' ', key = 'f', desc = 'Find File', action = ":lua Snacks.dashboard.pick('files')" },
+            { icon = ' ', key = 'g', desc = 'Find Text', action = ":lua Snacks.dashboard.pick('live_grep')" },
+            { icon = ' ', key = 'r', desc = 'Recent Files', action = ":lua Snacks.dashboard.pick('oldfiles')" },
+            {
+              icon = ' ',
+              key = 'c',
+              desc = 'Config',
+              action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+            },
+            { icon = ' ', key = 's', desc = 'Restore Session', section = 'session' },
+            { icon = '󰒲 ', key = 'L', desc = 'Lazy', action = ':Lazy', enabled = package.loaded.lazy ~= nil },
+            { icon = ' ', key = 'q', desc = 'Quit', action = ':qa' },
+          },
+        },
         sections = {
           {
             section = 'terminal',
@@ -18,9 +37,12 @@ return {
             padding = 1,
             width = 69,
           },
-          { icon = ' ', title = 'Keymaps', section = 'keys', indent = 2, padding = 1 },
-          { icon = ' ', title = 'Recent Files', section = 'recent_files', indent = 2, padding = 1 },
-          { icon = ' ', title = 'Projects', section = 'projects', indent = 2, padding = 1 },
+          -- { icon = ' ', title = 'Keymaps', section = 'keys', indent = 2, padding = 1 },
+          -- { icon = ' ', title = 'Recent Files', section = 'recent_files', indent = 2, padding = 1 },
+          -- { icon = ' ', title = 'Projects', section = 'projects', indent = 2, padding = 1 },
+          { section = 'keys', indent = 2, padding = 1 },
+          { section = 'recent_files', indent = 2, padding = 1 },
+          { section = 'projects', indent = 2, padding = 1 },
           { section = 'startup' },
         },
       },
