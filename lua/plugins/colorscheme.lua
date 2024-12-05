d452605cb64a062c3583b8334ccacded27a5829d
Ryan D. Najac <ryan.najac@columbia.edu>
2024-12-05 14:46:18 -0500

    modify colorscheme opts

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,7 +2,10 @@ return {
   'folke/tokyonight.nvim',
   opts = {
     transparent = true,
+    -- terminal_colors = false,
     styles = {
+      comments = { italic = true },
+      keywords = { bold = true },
       sidebars = 'transparent',
       -- floats = 'transparent',
     },
